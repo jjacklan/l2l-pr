37753ed68482d2c9e422b270147ebeeeba73e4ab
Josh Ackland <jjacklan@our.ecu.edu.au>
2020-09-18 15:39:09 +0800

    added name Josh. Fixed bug where pointer value was being used instead of variable value

--- l2l.c
+++ l2l.c
@@ -5,27 +5,27 @@
 #define CYAN "\e[38;96m"
 #define RED "\e[38;31m"
 
-void print_attendees(float*);
-void add_person(const char*, float*);
+void print_attendees(int*);
+void add_person(const char*, int*);
 
 int main(void) {
     printf("%sAttendees%s\n", GREEN, RESET);
-    float number_of_people = 0.0f;
+    int number_of_people = 0;   
     // Add a person
 
-    // add_person("Me", &number_of_people);
+    add_person("Josh", &number_of_people);
 
     // Print the number of attendees
     print_attendees(&number_of_people);
 }
 
-void add_person(const char* name, float* num_people) {
+void add_person(const char* name, int* num_people) {
     // Add to the number of people
     (*num_people)++;
-    printf("%i: %s%s%s\n", *(int*)num_people, CYAN, name, RESET);
+    printf("%i: %s%s%s\n",(int)*num_people, CYAN, name, RESET);
 }
 
-void print_attendees(float* maybe_number) {
+void print_attendees(int* maybe_number) {
     // Print the number of people as a whole number
-    printf("\nTotal: %s%i%s People\n", RED, *(int*)maybe_number, RESET);
+    printf("\nTotal: %s%i%s People\n", RED, (int)*maybe_number, RESET);
 }
